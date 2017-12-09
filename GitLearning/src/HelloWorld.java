0a38703f5069bea42abac76549dd9cf9d33fb4d1
subbu <subramanyam@subramanyam-SVF15213SNB>
2017-12-09 08:36:46 +0530

    jhhvkzvvsvssffs

---
+++ GitLearning/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+	
+	
+public static void display(){
+	
+	System.out.println("Hello world");
+	
+}
+
+}
